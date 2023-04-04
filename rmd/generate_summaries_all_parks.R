eecd9fd863328aac97bcd61d3054420b1e68c343
Kate Miller <kathryn_miller@nps.gov>
2023-04-04 14:02:10 -0400

    improvements for 2023

--- rmd/generate_summaries_all_parks.R
+++ rmd/generate_summaries_all_parks.R
@@ -6,8 +6,6 @@ library(purrr)
 library(knitr)
 library(rmarkdown)
 
-indir <- c("./rmd/")
-outdir <- c("./rmd/output/")
 
 #----- Generate water data files- must do each time new data are to be incorporated
 # library(NCRNWater)
@@ -16,11 +14,11 @@ outdir <- c("./rmd/output/")
 #                 export = TRUE, surface = TRUE, active_site = TRUE,
 #                 active_metric = TRUE, restricted = TRUE)
 
-#----- Reports for Lower NETN
+#----- Reports for Lower NETN with bands
 lnetn_parks <- c("MABI", "MIMA", "MORR", "ROVA", "SAGA", "SARA", "WEFA")
-render_LNETN_reports <- function(park_list, nutrient, plottype = 'bands'){
+render_LNETN_reports <- function(park_list, year = 2022, nutrient, plottype = 'bands'){
                           render(input = "./rmd/LNETN_water_summary_2023.Rmd",
-                          params = list(park = park_list, from = 2006, current = 2022, 
+                          params = list(park = park_list, from = 2006, current = year, 
                                         nutrient = nutrient, plottype = plottype),
                           output_file = paste0("NETN_water_summary_", 
                             park_list, "_", format(Sys.time(), '%b_%Y'), ".html"),
@@ -35,19 +33,31 @@ map(lnetn_parks[7], ~render_LNETN_reports(., nutrient = FALSE))
 
 render_LNETN_reports("SAIR", nutrient = FALSE, plottype = 'boxplot')
 
-#----- Report for ACAD
-render_ACAD_report <- function(park_list, year, nutrient){
-  render(input = paste0(indir, acad_rmd),
-         params = list(park = park_list, nutrient = nutrient, 
-                       from = 2006, to = year - 1, current = year),
+render_LNETN_reports_box <- function(park_list, year = 2022, 
+                                     nutrient = TRUE, 
+                                     plottype = 'boxplot'){
+  render(input = "./rmd/LNETN_water_summary_2023.Rmd",
+         params = list(park = park_list, from = 2006, current = year, 
+                       nutrient = nutrient, plottype = plottype),
          output_file = paste0("NETN_water_summary_", 
-                              park_list, "_", #"_2021", 
-                              format(Sys.time(), '%b_%Y'),
-                              ".html"),
-         output_dir = outdir,
+                              park_list, "_", format(Sys.time(), '%b_%Y'), 
+                              "_boxplots.html"),
+         output_dir = "./rmd/output/",
+         output_options = list(self_contained = TRUE))}
+
+map(lnetn_parks[1], ~render_LNETN_reports_box(., nutrient = TRUE))
+
+#----- Report for ACAD
+render_ACAD_report <- function(year, nutrient, plottype = 'bands'){
+  render(input = paste0(input = "./rmd/ACAD_water_summary_2023.Rmd"),
+         params = list(from = 2006, current = year, 
+                       nutrient = nutrient, plottype = plottype),
+         output_file = paste0("NETN_water_summary_ACAD_",
+                              format(Sys.time(), '%b_%Y'), ".html"),
+         output_dir = "./rmd/output/",
          output_options = list(self_contained = TRUE))
 }
-  
-render_ACAD_report("ACAD", 2022, nutrient = TRUE)
+
+render_ACAD_report(year = 2022, nutrient = TRUE, plottype = 'bands')
 
 
